6d4cf13dffe4d520313832292e5564ea9b6affc2
jundymek <jundymek@gmail.com>
2018-11-25 02:06:16 +0100

    Working CBV delete view

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from .views import IndexView, url_delete, url_refresh, modify_settings, registration_view, \
-    AddSiteToCheckView, check_all, update_email, SiteDetail, CreateNewUserView
+from .views import IndexView, url_refresh, modify_settings, AddSiteToCheckView, check_all, update_email, \
+    SiteDetailView, CreateNewUserView, SiteDeleteView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
@@ -9,8 +9,8 @@ urlpatterns = [
     path('add/', login_required(AddSiteToCheckView.as_view()), name='add'),
     path('update_email/', update_email, name='update_email'),
     path('check_all/', check_all, name='check_all'),
-    path('details/<int:pk>/', login_required(SiteDetail.as_view()), name='details'),
-    path('delete/<int:pk>/', url_delete, name='delete'),
+    path('details/<int:pk>/', login_required(SiteDetailView.as_view()), name='details'),
+    path('delete/<int:pk>/', login_required(SiteDeleteView.as_view()), name='delete'),
     path('refresh/<int:pk>/', url_refresh, name='refresh'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
